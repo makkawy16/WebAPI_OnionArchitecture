23057d03c4c96b0d378399c7abd7ba618512108d
makkawy16 <yousef.elmakkawy@gmail.com>
2025-06-26 21:07:04 +0300

    Add Signletone Regesteration

--- WebAPI_OnionArchitecture/Configurations/ServiceExtensions.cs
+++ WebAPI_OnionArchitecture/Configurations/ServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace WebAPI_OnionArchitecture.Configurations
+﻿using Contracts.RegisterServices;
+using System.Reflection;
+
+namespace WebAPI_OnionArchitecture.Configurations
 {
     public static class ServiceExtensions
     {
@@ -6,12 +9,41 @@
         public static void ConfigureCors(this IServiceCollection services)
         {
             //Todo Will Be Modifed based on requirments
-            services.AddCors( options => {
+            services.AddCors(options =>
+            {
                 options.AddPolicy("CorsPolicy", builder =>
                 builder.AllowAnyOrigin()
                 .AllowAnyHeader()
                 .AllowAnyMethod());
-                });
+            });
+        }
+
+        /// <summary>
+        /// Registers all services implementing <see cref="ISingletone"/> from the specified assemblies 
+        /// into the dependency injection container with a transient lifetime. Each class is registered 
+        /// for all interfaces it implements, as well as itself.
+        /// </summary>
+        public static void ConfigureISingletoneService(this IServiceCollection services)
+        {
+            Assembly targetAssemply = Assembly.Load("Contracts");
+            Type interfaceType = typeof(ISingletone);
+
+            //Get Any thing implements ISingletone
+            IEnumerable<Type> singletoneServices = targetAssemply.GetTypes()
+                .Where(t => interfaceType.IsAssignableFrom(t) && t.IsClass && !t.IsAbstract);
+
+            foreach (Type serviceType in singletoneServices)
+            {
+                IEnumerable<Type> interfaces = serviceType.GetInterfaces().Where(i => i != typeof(ISingletone));
+
+                foreach (Type serviceIntefarce in interfaces)
+                {
+                    // Register the class type for each interface it implements
+                    services.AddSingleton(serviceIntefarce, serviceType);
+                }
+                // Register the class itself
+                services.AddSingleton(serviceType);
+            }
         }
     }
 }
--- WebAPI_OnionArchitecture/Program.cs
+++ WebAPI_OnionArchitecture/Program.cs
@@ -13,6 +13,8 @@ namespace WebAPI_OnionArchitecture
 
             builder.Services.ConfigureCors();
 
+
+            builder.Services.ConfigureISingletoneService();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
