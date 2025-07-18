c4d67c9b419a4fb76e754aa9809f58f00ea93117
Yousef Elmakkawy <111737169+makkawy16@users.noreply.github.com>
2025-07-18 18:42:20 +0300

    3.2 creating models 3.3 db context (#4)
    * Add Company and employee aas entities
    
    * install required packages for EF
    
    * Add Conection string , create dbcontext and add first migration
    
    * add entity configuration files
    
    * remove comment
    
    * create new migration with mock data

---
+++ Repository/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository.Configurations;
+
+namespace Repository.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new CompanyEntityTypeConfigurations());
+            builder.ApplyConfiguration(new EmployeeEntityTypeConfigurations());
+        }
+
+    }
+}
