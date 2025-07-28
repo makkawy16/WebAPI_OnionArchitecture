ff9eceacf7c5299fe223d21dbf546d017ea8b082
Yousef Elmakkawy <111737169+makkawy16@users.noreply.github.com>
2025-07-29 00:07:47 +0300

    Add register service Iscoped (#6)
    * add interface and register any interface implements it
    
    * register it in the program

--- WebAPI_OnionArchitecture/Configurations/ServiceExtensions.cs
+++ WebAPI_OnionArchitecture/Configurations/ServiceExtensions.cs
@@ -1,6 +1,5 @@
-﻿using Contracts.RegisterServices;
-using System.Reflection;
-
+﻿using System.Reflection;
+using Contracts.RegisterServices;
 namespace WebAPI_OnionArchitecture.Configurations
 {
     public static class ServiceExtensions
@@ -49,6 +48,42 @@ namespace WebAPI_OnionArchitecture.Configurations
                     }
                     // Register the class itself
                     services.AddSingleton(serviceType);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Registers all services implementing <see cref="IScopedService"/> from the specified assemblies 
+        /// into the dependency injection container with a transient lifetime. Each class is registered 
+        /// for all interfaces it implements, as well as itself.
+        /// </summary>
+        public static void ConfigureIScopedService(this IServiceCollection services)
+        {
+            // Assembly targetAssemply = Assembly.Load("LoggerService");  
+            Type interfaceType = typeof(IScopedService);
+
+            Assembly[] assemblies = {
+                                    Assembly.Load("Repository")
+                                };
+
+            foreach (Assembly targetAssemply in assemblies)
+            {
+                //Get Any thing implements ISingletone
+                IEnumerable<Type> scopedServices = targetAssemply.GetTypes()
+                    .Where(t => interfaceType.IsAssignableFrom(t) && t.IsClass && !t.IsAbstract);
+
+                foreach (Type serviceType in scopedServices)
+                {
+                    IEnumerable<Type> interfaces = serviceType.GetInterfaces().Where(i => i != typeof(IScopedService));
+
+                    foreach (Type serviceIntefarce in interfaces)
+                    {
+                        // Register the class type for each interface it implements
+                        services.AddScoped(serviceIntefarce, serviceType);
+                    }
+                    // Register the class itself
+                    services.AddScoped(serviceType);
                 }
             }
         }
